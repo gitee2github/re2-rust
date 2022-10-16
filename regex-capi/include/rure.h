5a1652452910e9e6024dc67a313e4ca517084b91
yangwentong <425822674@qq.com>
2022-10-16 14:21:34 +0800

    CheckRewriteString改为由Rust实现

--- regex-capi/include/rure.h
+++ regex-capi/include/rure.h
@@ -611,6 +611,8 @@ const char *rure_replace_all(rure *re, const uint8_t *haystack, size_t len_h,
 rure *rure_new(const uint8_t *pattern, size_t length);
 bool rure_consume(rure *re, const uint8_t *haystack, size_t length, rure_match *match);
 int rure_max_submatch(const char *rewrite);
+bool rure_check_rewrite_string(const char *rewrite, int max_token);
+
 
 #ifdef __cplusplus
 }
